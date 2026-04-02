092f46918ccffc45b07674f05af6404e4db22bd8
jean viven <jeanvivien.sicot@lapiscine.pro>
2026-04-02 12:35:39 +0200

    derive macro

--- derive_macros/src/lib.rs
+++ derive_macros/src/lib.rs
@@ -1,15 +1,62 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use syn::{DeriveInput, parse_macro_input};
+
+use syn::{Data, Fields};
 
 #[proc_macro_derive(Builder)]
 pub fn builder_derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
-    let name = &input.ident; // nom du struct — "Task"
-    
-    let expanded = quote! {
-        // code généré ici
+    let name = &input.ident;
+
+    // Extraire les champs
+    let fields = match &input.data {
+        Data::Struct(data) => match &data.fields {
+            Fields::Named(fields) => &fields.named,
+            _ => panic!("Builder only supports named fields"),
+        },
+        _ => panic!("Builder only supports structs"),
     };
-    
+
+    // Noms et types des champs
+    let field_names: Vec<_> = fields.iter().map(|f| &f.ident).collect();
+
+    let field_types: Vec<_> = fields.iter().map(|f| &f.ty).collect();
+
+    let builder_name = quote::format_ident!("{}Builder", name);
+
+    let expanded = quote! {
+        pub struct #builder_name {
+            #( #field_names: Option<#field_types> ),*
+        }
+
+        impl #builder_name {
+            pub fn new() -> Self {
+                Self {
+                    #( #field_names: None ),*
+                }
+            }
+
+            #(
+                pub fn #field_names(mut self, val: #field_types) -> Self {
+                    self.#field_names = Some(val);
+                    self
+                }
+            )*
+
+            pub fn build(self) -> #name {
+                #name {
+                    #( #field_names: self.#field_names.unwrap() ),*
+                }
+            }
+        }
+
+        impl #name {
+            pub fn builder() -> #builder_name {
+                #builder_name::new()
+            }
+        }
+    };
+
     TokenStream::from(expanded)
 }
--- domain/src/lib.rs
+++ domain/src/lib.rs
@@ -2,15 +2,34 @@ pub mod error;
 pub mod task;
 pub mod user;
 
-pub use task::Task;
 pub use task::NewTask;
-pub use task::UpdateTask;
+pub use task::Task;
 pub use task::TaskRepository;
+pub use task::UpdateTask;
 
-pub use user::User;
 pub use user::NewUser;
+pub use user::User;
 pub use user::UserAuth;
 pub use user::UserRegister;
 pub use user::UserRepository;
 
 pub use error::RepositoryError;
+
+#[cfg(test)]
+mod tests {
+    use derive_macros::Builder;
+
+    #[derive(Builder)]
+    struct Point {
+        x: i32,
+        y: i32,
+    }
+
+    #[test]
+    fn test_builder() {
+        let point = Point::builder().x(10).y(20).build();
+
+        assert_eq!(point.x, 10);
+        assert_eq!(point.y, 20);
+    }
+}

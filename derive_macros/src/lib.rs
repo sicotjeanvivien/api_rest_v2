ef77e88287fd4583dd2b73e660e51b3de01a08fa
jean viven <jeanvivien.sicot@lapiscine.pro>
2026-04-02 09:48:17 +0200

    start derive macro

---
+++ derive_macros/src/lib.rs
@@ -0,0 +1,15 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
+
+#[proc_macro_derive(Builder)]
+pub fn builder_derive(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let name = &input.ident; // nom du struct — "Task"
+    
+    let expanded = quote! {
+        // code généré ici
+    };
+    
+    TokenStream::from(expanded)
+}
